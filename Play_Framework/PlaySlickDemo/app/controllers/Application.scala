441a1800930b7458d12e4a1fe9070120fc581467
Rashmit Rathod <rashmit.rathod@yahoo.com>
2015-10-01 12:32:20 +0530

    Updated UI and relaed source code

--- Play_Framework/PlaySlickDemo/app/controllers/Application.scala
+++ Play_Framework/PlaySlickDemo/app/controllers/Application.scala
@@ -1,43 +1,94 @@
 package controllers
 
-import models.Task
+import models.database.DBUtils
 import play.api.data.Form
 import play.api.data.Forms.nonEmptyText
 import play.api.mvc.Action
 import play.api.mvc.Controller
-import models.database.EmployeeUtil
+import play.api.Logger
+import models.EmployeeModel
+import models.DepartmentModel
 
 object Application extends Controller {
 
   def index = Action {
-    Redirect(routes.Application.tasks)
+    
+    //DBUtils.setup
+    
+    
+    Ok(views.html.index())
   }
-   /* we use TODO to define our action implementations. Because we don’t 
-   want to write the action implementations yet, we can use the built-in TODO 
-   action that will return a 501 Not Implemented HTTP response. */
-  
-  def newTask = Action { implicit request =>
-  taskForm.bindFromRequest.fold(
-    errors => BadRequest(views.html.index(Task.all, errors)),
-    label => {
-      println("Before insert")
-      EmployeeUtil.insertEmployees
-      println("After insert")
-      EmployeeUtil.getEmployees
-     // Task.create(label)
-      Redirect(routes.Application.tasks)
-    }
-  )
-}
+                                         
+  def allManagers = Action { 
+                                          
+      println("In allManagers..")
+      
+     val managers= DBUtils.getAllManagers    
+     
+     //Logger.debug(managers.mkString("\n"))
 
-  def deleteTask(id:  Int) = TODO
+     
+     println("After setup")
+    
+     Ok(views.html.managerPage(managers: List[models.EmployeeModel]))
+    }
+  
+  def allDepartments = Action {
+    
+    println("In allDepartments ")
+    
+    //val deptList=DBUtils.getAllDepartments
+    val deptList= DBUtils.getAllDepartments
+     deptList.foreach(d => println(d.id+" "+d.departmentName))
+    
+     
+    Logger.debug(deptList.mkString("\n"))
+    //Ok(views.html.query(deptList))
+     
+    Ok(views.html.allDepartmentPage(deptList: List[models.DepartmentModel]))
+  }
+  
+  def allReportee(managerId: String) = Action{
+    
+    val repoteeList = DBUtils.getAllReportee(managerId)
+    
+    
+    
+    Ok(views.html.manageWiseReportees(repoteeList : List[models.EmployeeModel]))
+  }
+  
+  
+  def managerByDepartment(departmentName: String, id:String)= Action{
+    
+         val mbydept= DBUtils.getManagerByDepartment(departmentName, id)
+    
+    Ok(views.html.managerPage(mbydept:List[models.EmployeeModel]))
+  }
+  
+  def employeeById(id:String, deptId:String) = Action{
+    
+    val empDesc = DBUtils.getEmployeeDetails(id, deptId)
+    
+    Ok(views.html.employeeByIdDescription(empDesc: List[models.EmployeeModel]))
+  }
+  
+  
   
   val taskForm = Form("label" -> nonEmptyText)
+                                           // )
   
-  
-  def tasks= Action{
-    Ok(views.html.index(Task.all,taskForm))
+  def allEmployees = Action {
+    
+     val emp= DBUtils.getAllEmployees
+     
+     Ok(views.html.employeeByIdDescription(emp: List[models.EmployeeModel]))
+    
   }
   
-
+  
+ 
+  
+  
+  
+  
 }
---
+++ Play_Framework/PlaySlickDemo/app/models/DepartmentModel.scala
@@ -0,0 +1,3 @@
+package models
+
+case class DepartmentModel(val id:Long, val departmentName: String)
